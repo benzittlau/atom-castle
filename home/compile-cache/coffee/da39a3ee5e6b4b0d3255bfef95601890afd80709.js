d4c7cb979b092a2290a05625bbc0530734feadca
Ben Zittlau <ben@zittlau.ca>
2016-08-07 11:45:01 -0600

    Fixing the folder structure for linking.

--- home/compile-cache/coffee/da39a3ee5e6b4b0d3255bfef95601890afd80709.js
+++
@@ -1,8 +0,0 @@
-(function() {
-
-
-}).call(this);
-
-//# sourceMappingURL=data:application/json;base64,ewogICJ2ZXJzaW9uIjogMywKICAiZmlsZSI6ICIiLAogICJzb3VyY2VSb290IjogIiIsCiAgInNvdXJjZXMiOiBbCiAgICAiL1VzZXJzL2J6aXR0bGF1Ly5hdG9tL3BhY2thZ2VzL3NldGktdWkvZmlsZS10eXBlcy9Db2ZmZVNjcmlwdC5jb2ZmZWUiCiAgXSwKICAibmFtZXMiOiBbXSwKICAibWFwcGluZ3MiOiAiQUFBQTtBQUFBO0FBQUEiCn0=
-
-//# sourceURL=/Users/bzittlau/.atom/packages/seti-ui/file-types/CoffeScript.coffee
